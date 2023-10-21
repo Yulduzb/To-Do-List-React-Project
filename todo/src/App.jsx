d9a2e138f6b45242b77e349feef66d68977d86ec
Yulduz Buranbaeva <130300539+Yulduzb@users.noreply.github.com>
2023-10-21 12:28:58 +0300

    json-server ile  kendi ağimizda veri tabani prototip oluşturuldu ve axiosla veri tabanından veriler çekildı.

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,32 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
-
+  const [todos,setTodos]=useState(null);
+useEffect(()=>{
+  axios.get('http://localhost:3000/todos')
+  .then((res)=>setTodos(res.data))
+},[]);
   return (
 
-    <div>selam</div>
+    <div className='container py-5'>
+      <h3>Yapılacaklar Listesı</h3>
+      <form className='d-flex justify-content-center gap-4 my-5'>
+        <input type="text" className='form-control shadow' />
+        <button className='btn btn-primary shadow '>Gönder</button>
+      </form>
+
+      <ul>
+        {/*Apidan cevap beklerken.Eğer null isa Yülleniyor yazar */}
+        {todos===null && <p>Yükleniyor</p>}
+        {/*eğer todo true isa  todos dizisini map ile döner ve listeler */}
+        {todos?.map((todo)=>
+        <li>{todo.id}</li>)}
+      </ul>
+
+
+
+    </div>
   )
 }
 
